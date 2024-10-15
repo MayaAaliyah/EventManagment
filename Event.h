6a5f1059bc7b38b57d9da431157147035cad67dd
MayaAaliyah <fosterma@uab.edu>
2024-10-14 22:28:58 -0500

    Add files via upload

---
+++ Event.h
@@ -0,0 +1,27 @@
+#pragma once 
+#include <string>
+#include <iostream> 
+#include <vector>
+using namespace std;
+
+class Event
+{
+protected:
+	string eventName;
+	string eventDate;
+	string location;
+
+
+
+public: 
+	Event(string& eName, string& eDate, string& eLocation)
+		: eventName(eName), eventDate(eDate), location(eLocation){}
+
+	//virtual function 
+	virtual void getDetails() {
+		cout << "Event: " << eventName << endl;
+		cout << "Date: " << eventDate << endl;
+		cout << "Location: " << location << endl;
+ 	}
+};
+
